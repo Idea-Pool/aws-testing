c1780e18378f0c30e9836e53128161bbf216f3d6
szikszail <sziklaszlo@gmail.com>
2023-05-24 15:17:43 +0200

    Fixing theme switcher flicker

--- docs/assets/scripts/theme-switcher.js
+++ docs/assets/scripts/theme-switcher.js
@@ -2,25 +2,28 @@
     const ICONS = {
         'light': '<i class="fa-solid fa-sun">&nbsp;</i>',
         'dark': '<i class="fa-solid fa-moon">&nbsp;</i>',
+        'default': '<i class="fa-solid fa-moon">&nbsp;</i>',
     };
     const SST_KEY = 'jtd-theme';
-    const DEFAULT_THEME = 'dark';
 
     function initThemeSwitcher() {
         const toggleDarkMode = document.querySelector('.js-toggle-dark-mode');
 
-        function setTheme(theme) {
-            if (theme === 'default') {
-                theme = DEFAULT_THEME;
-            } else {
-                jtd.setTheme(theme);
+        const originalSetThem = jtd.setTheme;
+        jtd.setTheme = function (theme) {
+            if (jtd.getTheme() !== theme) {
+                originalSetThem(theme);
             }
+        }
+    
+        function setTheme(theme) {
+            jtd.setTheme(theme);
             sst.setItem(SST_KEY, theme);
             toggleDarkMode.innerHTML = ICONS[theme];
         }
 
         function toggleTheme(currentTheme) {
-            setTheme(currentTheme == 'dark' ? 'light' : 'dark');
+            setTheme(currentTheme === 'light' ? 'dark' : 'light');
         }
 
         jtd.addEvent(toggleDarkMode, 'click', function () {
