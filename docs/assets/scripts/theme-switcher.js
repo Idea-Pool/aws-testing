fa77b91d376f764d11f97293b52441949647e081
szikszail <sziklaszlo@gmail.com>
2023-05-24 14:04:37 +0200

    Added handler for default theme

--- docs/assets/scripts/theme-switcher.js
+++ docs/assets/scripts/theme-switcher.js
@@ -4,11 +4,15 @@
         'dark': '<i class="fa-solid fa-moon">&nbsp;</i>',
     };
     const SST_KEY = 'jtd-theme';
+    const DEFAULT_THEME = 'dark';
 
     function initThemeSwitcher() {
         const toggleDarkMode = document.querySelector('.js-toggle-dark-mode');
 
         function setTheme(theme) {
+            if (theme === 'default') {
+                theme = DEFAULT_THEME;
+            }
             jtd.setTheme(theme);
             sst.setItem(SST_KEY, theme);
             toggleDarkMode.innerHTML = ICONS[theme];
